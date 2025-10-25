5ed5a2ca1d80374ec2c17cbdfbcb9d2e2b7265f5
cells-OSS <open-source-version@proton.me>
2025-10-25 22:28:35 +0300

    Update on the UI
    Added an 'entry'.

--- tester.py
+++ tester.py
@@ -4,15 +4,25 @@ window = Tk()
 
 window.title("Pyclicker")
 window.geometry("400x450")
-icon = PhotoImage(file='Untitled.png')
-window.iconphoto(True,icon)
+icon = PhotoImage(file='icon.png')
+window.iconphoto(True, icon)
+window.config(bg="#525252")
 
-window.config(background="#525252")
-labelSlogan = Label(window,text="The only clicker software you'll need.",background="#525252",fg="white",)
+labelSlogan = Label(window,text="The only clicker software you'll need.", background="#525252", fg="white")
 labelSlogan.pack()
-labelSlogan.place(x=80,y=30)
-button = Button(window,text='Start')
-button.pack()
+labelSlogan.place(x = 80, y = 30)
+
+startButton = Button(window, text='Start', bg="#525252", fg="white", activebackground='#726248', activeforeground='white')
+startButton.pack()
+startButton.place(x=143, y =110)
+
+stopButton = Button(window, text='Stop', bg="#525252", fg="white", activebackground='#726248', activeforeground='white')
+stopButton.pack()
+stopButton.place(x = 207, y =110)
+
+secondsEntry = Entry(bg = "#726248", fg = "white")
+secondsEntry.pack
+secondsEntry.place(x = 120, y = 80)
 
 window.mainloop()
 
